2c8033162c280dccfc9df5226c90c26aed6ab1df
Ryan Procter <ryanprocter1994@hotmail.co.uk>
2024-03-26 21:13:40 +0000

    wording changed

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+# Ask user name
+# Store user name
+# Print user name
+# Ask user age
+# Store user age
+# Print user age
+# Print Hello World
+
+# user_name = input("What is your name? ")
+# print(user_name)
+# user_age = input("What is your age? ")
+# print(user_age)
+print("Git is awesome!")
